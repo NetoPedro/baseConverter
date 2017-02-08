413c1dca5167f73748303948624b68dca0eb948f
Fábio André Vilar Lourenço <1150434@isep.ipp.pt>
2017-02-08 00:54:42 +0000

    Main acitivity layout

--- app/src/main/java/com/trimteam/baseconverter/MainActivity.java
+++ app/src/main/java/com/trimteam/baseconverter/MainActivity.java
@@ -2,6 +2,7 @@ package com.trimteam.baseconverter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void fillSpinner(){
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+
     }
 }
