aff57b51be16cb2a4039c5d88e707497168aab4c
NetoPedro <pedroneto_09@hotmail.com>
2017-02-08 17:53:47 +0000

    Alteração no tipo de dados

--- app/src/main/java/com/trimteam/baseconverter/Converter.java
+++ app/src/main/java/com/trimteam/baseconverter/Converter.java
@@ -9,73 +9,76 @@ public class Converter {
 
 //   private int [] values  = {0,1,2,3,4,5,6,7,8,9,10,'A','B','C','D','E','F'};
 
-    public String convertDecimalBinary(int decimal){
-        return Integer.toBinaryString(decimal);
+    public static String convertDecimalBinary(long decimal){
+        return Long.toBinaryString(decimal);
     }
-    public String convertDecimalOctal(int decimal){
-        return Integer.toOctalString(decimal);
+    public static String convertDecimalOctal(long decimal){
+        return Long.toOctalString(decimal);
     }
-    public String convertDecimalHexa(int decimal){
-        return Integer.toHexString(decimal);
+    public static String convertDecimalHexa(long decimal){
+        return Long.toHexString(decimal);
     }
 
-    public String convertBinaryDecimal(int binary){
-        String binaryString = Integer.toString(binary);
-        int decimalString = 0;
+    public static String convertBinaryDecimal(long binary){
+        String binaryString = Long.toString(binary);
+        long decimalString = 0;
+        binaryString = new StringBuilder(binaryString).reverse().toString();
         for(int i  = 0;  i < binaryString.length(); i++){
-            decimalString += Integer.getInteger(binaryString.charAt(i) + "") * Math.pow(2,i);
+            decimalString += Long.valueOf(binaryString.charAt(i) + "") * Math.pow(2,i);
         }
-        return Integer.toString(decimalString);
+        return Long.toString(decimalString);
     }
 
-    public String convertBinaryOctal(int binary){
-        int decimal = Integer.getInteger(convertBinaryDecimal(binary));
+    public static String convertBinaryOctal(long binary){
+        long decimal = Long.valueOf(convertBinaryDecimal(binary));
         return convertDecimalOctal(decimal);
     }
 
-    public String convertBinaryHexa(int binary){
-        int decimal = Integer.getInteger(convertBinaryDecimal(binary));
+    public static String convertBinaryHexa(long binary){
+        long decimal = Long.valueOf(convertBinaryDecimal(binary));
         return convertDecimalHexa(decimal);
     }
 
-    public String convertOctalDecimal(int octal){
-        String octalString = Integer.toString(octal);
-        int decimal = 0 ;
+    public static String convertOctalDecimal(long octal){
+        String octalString = Long.toString(octal);
+        long decimal = 0 ;
+        octalString = new StringBuilder(octalString).reverse().toString();
         for(int i = 0 ; i<octalString.length(); i++){
-            decimal += Integer.getInteger((octalString.charAt(i) + "")) * Math.pow(8,i);
+            decimal += Long.valueOf((octalString.charAt(i) + "")) * Math.pow(8,i);
         }
-        return Integer.toString(decimal);
+        return Long.toString(decimal);
     }
 
-    public String convertOctalBinary(int octal){
-        int decimal = Integer.getInteger(convertOctalDecimal(octal));
+    public static String convertOctalBinary(long octal){
+        long decimal = Long.valueOf(convertOctalDecimal(octal));
         return convertDecimalBinary(decimal);
     }
 
-    public String convertOctalHexa(int octal){
-        int decimal = Integer.getInteger(convertOctalDecimal(octal));
+    public static String convertOctalHexa(long octal){
+        long decimal = Long.valueOf(convertOctalDecimal(octal));
         return convertDecimalHexa(decimal);
     }
 
-    public String convertHexaDecimal(String hexa){
-        int decimal = 0;
+    public static String convertHexaDecimal(String hexa){
+        long decimal = 0;
+        hexa =new StringBuilder(hexa).reverse().toString();
         for(int i = 0 ; i < hexa.length() ; i++){
             decimal += findIndice(hexa.charAt(i)) * Math.pow(16,i);
         }
-        return Integer.toString(decimal);
+        return Long.toString(decimal);
     }
 
-    public String convertHexaOctal(String hexa){
+    public static String convertHexaOctal(String hexa){
         String decimal = convertHexaDecimal(hexa);
-        return convertDecimalOctal(Integer.valueOf(decimal));
+        return convertDecimalOctal(Long.valueOf(decimal));
     }
 
-    public String convertHexaBinary(String hexa){
+    public static String convertHexaBinary(String hexa){
         String decimal = convertHexaDecimal(hexa);
-        return convertDecimalBinary(Integer.valueOf(decimal));
+        return convertDecimalBinary(Long.valueOf(decimal));
     }
 
-    private int findIndice(char indice){
+    private static int findIndice(char indice){
 
         switch (indice){
             case 'A' : return 10;
